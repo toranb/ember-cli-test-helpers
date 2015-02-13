1206aa04aaafb4418c7d1b3ba32ffb1ef869133c
Brandon Williams <williamsbdev@gmail.com>
2015-02-12 21:13:29 -0600

    [TEST]: updating test to be QUnit 2.0 compliant

--- tests/acceptance/usage-test.js
+++ tests/acceptance/usage-test.js
@@ -30,63 +30,63 @@ module("Acceptance: Usage", {
     }
 });
 
-test("input with class text-input is an with type text", function() {
+test("input with class text-input is an with type text", function(assert) {
     visit("/");
     andThen(function() {
         isTextInput("input.text-input");
     });
 });
 
-test("input with class email-input is an input with type email", function() {
+test("input with class email-input is an input with type email", function(assert) {
     visit("/");
     andThen(function() {
         isEmailInput("input.email-input");
     });
 });
 
-test("input with class password-input is an input with type password", function() {
+test("input with class password-input is an input with type password", function(assert) {
     visit("/");
     andThen(function() {
         isPasswordInput("input.password-input");
     });
 });
 
-test("input with class tel-input is an input with type tel", function() {
+test("input with class tel-input is an input with type tel", function(assert) {
     visit("/");
     andThen(function() {
         isTelInput("input.tel-input");
     });
 });
 
-test("input with class hidden-input is an input with type hidden", function() {
+test("input with class hidden-input is an input with type hidden", function(assert) {
     visit("/");
     andThen(function() {
         isHiddenInput("hidden-input", "");
     });
 });
 
-test("input with class checkbox-input is an input with type checkbox", function() {
+test("input with class checkbox-input is an input with type checkbox", function(assert) {
     visit("/");
     andThen(function() {
         isCheckbox(CHECKBOX_INPUT);
     });
 });
 
-test("inputs with class radio-button are an input with type radio and number of radio buttons is verified", function() {
+test("inputs with class radio-button are an input with type radio and number of radio buttons is verified", function(assert) {
     visit("/");
     andThen(function() {
         isRadioButton(RADIO_BUTTON, 2);
     });
 });
 
-test("input with class radio-button-text is an input with type radio and label with specified text", function() {
+test("input with class radio-button-text is an input with type radio and label with specified text", function(assert) {
     visit("/");
     andThen(function() {
         isRadioButtonWithText("input.radio-button-text", "Radio Button Text");
     });
 });
 
-test("input with class text-button is an input with type button with specified text and optional type (defaulting to type=button)", function() {
+test("input with class text-button is an input with type button with specified text and optional type (defaulting to type=button)", function(assert) {
     visit("/");
     andThen(function() {
         isTextButton("button.text-button", "Text Button");
@@ -94,7 +94,7 @@ test("input with class text-button is an input with type button with specified t
     });
 });
 
-test("link with class link is an anchor tag with specified text and optional href", function() {
+test("link with class link is an anchor tag with specified text and optional href", function(assert) {
     visit("/");
     andThen(function() {
         isLink(".link", "link to foo");
@@ -102,57 +102,57 @@ test("link with class link is an anchor tag with specified text and optional hre
     });
 });
 
-test("input with class focus-input is an input with the focus set", function() {
+test("input with class focus-input is an input with the focus set", function(assert) {
     visit("/");
     andThen(function() {
         isFocused("input.focus-input");
     });
 });
 
-test("selectRadioButton helper will select given radio button with provided selector and value", function() {
+test("selectRadioButton helper will select given radio button with provided selector and value", function(assert) {
     visit("/");
     selectRadioButton(RADIO_BUTTON, "foo");
     andThen(function() {
-        equal(find(RADIO_BUTTON + ":eq(0)").prop("checked"), true);
-        equal(find(RADIO_BUTTON + ":eq(1)").prop("checked"), false);
+        assert.equal(find(RADIO_BUTTON + ":eq(0)").prop("checked"), true);
+        assert.equal(find(RADIO_BUTTON + ":eq(1)").prop("checked"), false);
     });
 });
 
-test("selectRadioButton helper will unselect all radio buttons for selector", function() {
+test("selectRadioButton helper will unselect all radio buttons for selector", function(assert) {
     visit("/");
     selectRadioButton(RADIO_BUTTON, "foo");
     andThen(function() {
-        equal(find(RADIO_BUTTON + ":eq(0)").prop("checked"), true);
-        equal(find(RADIO_BUTTON + ":eq(1)").prop("checked"), false);
+        assert.equal(find(RADIO_BUTTON + ":eq(0)").prop("checked"), true);
+        assert.equal(find(RADIO_BUTTON + ":eq(1)").prop("checked"), false);
     });
     selectRadioButton(RADIO_BUTTON);
     andThen(function() {
-        equal(find(RADIO_BUTTON).length, 2);
-        equal(find(RADIO_BUTTON + ":eq(0)").prop("checked"), false);
-        equal(find(RADIO_BUTTON + ":eq(1)").prop("checked"), false);
+        assert.equal(find(RADIO_BUTTON).length, 2);
+        assert.equal(find(RADIO_BUTTON + ":eq(0)").prop("checked"), false);
+        assert.equal(find(RADIO_BUTTON + ":eq(1)").prop("checked"), false);
     });
 });
 
-test("setCheckboxChecked will check or uncheck the checkbox for given selector based on specified checked value", function() {
+test("setCheckboxChecked will check or uncheck the checkbox for given selector based on specified checked value", function(assert) {
     visit("/");
     // checkbox is initially unchecked
     andThen(function() {
-        equal(find(CHECKBOX_INPUT).prop("checked"), false);
+        assert.equal(find(CHECKBOX_INPUT).prop("checked"), false);
     });
     // checkbox is checked
     setCheckboxChecked(CHECKBOX_INPUT, true);
     andThen(function() {
-        equal(find(CHECKBOX_INPUT).prop("checked"), true);
+        assert.equal(find(CHECKBOX_INPUT).prop("checked"), true);
     });
     // checkbox will remain checked
     setCheckboxChecked(CHECKBOX_INPUT, true);
     andThen(function() {
-        equal(find(CHECKBOX_INPUT).prop("checked"), true);
+        assert.equal(find(CHECKBOX_INPUT).prop("checked"), true);
     });
     // checkbox will be unchecked
     setCheckboxChecked(CHECKBOX_INPUT, false);
     andThen(function() {
-        equal(find(CHECKBOX_INPUT).prop("checked"), false);
+        assert.equal(find(CHECKBOX_INPUT).prop("checked"), false);
     });
 });
 
