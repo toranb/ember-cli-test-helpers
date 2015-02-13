1fbd5cd25f9c4575cbc41e2b642d57a68e400404
Brandon Williams <williamsbdev@gmail.com>
2015-02-12 20:36:52 -0600

    [ENHANCEMENT]: remove requests from unfired and unhandled when it is incorrect

--- addon/tests/helpers/test-done.js
+++ addon/tests/helpers/test-done.js
@@ -2,76 +2,92 @@ import Ember from "ember";
 
 var TestDone = Ember.Object.extend({
     testDoneCallback: function() {
-        if(this.get("allThree")) {
-            this.incorrectlyMocked();
-            this.overMocked();
-            this.underMocked();
+        var lists = this._configureLists();
+        var incorrect = lists.incorrect;
+        var unfired = lists.unfired;
+        var unhandled = lists.unhandled;
+        if(this._allThree(lists)) {
+            this._logIncorrectRequests(incorrect);
+            this._logUnfiredRequests(unfired);
+            this._logUnhandledRequests(unhandled);
             return "incorrect, unfired, and unhandled";
         }
-        if(this.get("incorrect")) {
-            this.incorrectlyMocked();
+        if(incorrect.length) {
+            this._logIncorrectRequests(incorrect);
             return "incorrect";
         }
-        if(this.get("unfired") && this.get("unhandled")) {
-            this.overMocked();
-            this.underMocked();
+        if(unfired.length && unhandled.length) {
+            this._logUnfiredRequests(unfired);
+            this._logUnhandledRequests(unhandled);
             return "unfired and unhandled";
         }
-        if(this.get("unfired")) {
-            this.overMocked();
+        if(unfired.length) {
+            this._logUnfiredRequests(unfired);
             return "unfired";
         }
-        if(this.get("unhandled")) {
-            this.underMocked();
+        if(unhandled.length) {
+            this._logUnhandledRequests(unhandled);
             return "unhandled";
         }
     },
-    incorrectMocks: function() {
-        var incorrect = [];
-        $.fauxjax.unfired().forEach(function(unfired){
-            $.fauxjax.unhandled().forEach(function(unhandled){
-                if(unfired.url === unhandled.url && unfired.type === unhandled.type) {
-                    incorrect.push({
+    _configureLists: function() {
+        var lists = {
+            incorrect: [],
+            unhandled: [],
+            unfired: []
+        };
+        if($.fauxjax.unfired().length > 0 && $.fauxjax.unhandled().length > 0) {
+            $.fauxjax.unfired().forEach(function(unfired){
+                var found = $.fauxjax.unhandled().filter(function(unhandled){
+                    return unfired.url === unhandled.url &&
+                        unfired.type === unhandled.type;
+                });
+                if(found.length === 0) {
+                    lists.unfired.push(unfired);
+                } else {
+                    lists.incorrect.push({
                         unfired: unfired,
-                        unhandled: unhandled
+                        unhandled: found[0]
                     });
                 }
             });
-        });
-        return incorrect;
-    }.property(),
-    allThree: function() {
-        var incorrect = this.get("incorrect");
-        return incorrect &&
-            (incorrect !== this.get("unfired") ||
-             incorrect !== this.get("unhandled"));
-    }.property(),
-    incorrect: function() {
-        return this.get("incorrectMocks").length;
-    }.property(),
-    unhandled: function() {
-        return $.fauxjax.unhandled().length;
-    }.property(),
-    unfired: function() {
-        return $.fauxjax.unfired().length;
-    }.property(),
-    overMocked: function() {
+            $.fauxjax.unhandled().forEach(function(unhandled){
+                var found = $.fauxjax.unfired().filter(function(unfired){
+                    return unfired.url === unhandled.url &&
+                        unfired.type === unhandled.type;
+                });
+                if(found.length === 0) {
+                    lists.unhandled.push(unhandled);
+                }
+            });
+        } else {
+            lists.unfired = $.fauxjax.unfired();
+            lists.unhandled = $.fauxjax.unhandled();
+        }
+        return lists;
+    },
+    _allThree: function(lists) {
+        return lists.incorrect.length > 0 &&
+            lists.unfired.length > 0 &&
+            lists.unhandled.length > 0;
+    },
+    _logUnfiredRequests: function(unfired) {
         var self = this;
-        $.fauxjax.unfired().forEach(function(request){
-            self.createMessage(request, "FIRED");
+        unfired.forEach(function(request){
+            self._createMessage(request, "FIRED");
         });
         ok(false, "Overmocked requests for %@.".fmt(this.get("name")));
     },
-    underMocked: function() {
+    _logUnhandledRequests: function(unhandled) {
         var self = this;
-        $.fauxjax.unhandled().forEach(function(request){
-            self.createMessage(request, "MOCKED");
+        unhandled.forEach(function(request){
+            self._createMessage(request, "MOCKED");
         });
         ok(false, "Unmocked requests for %@.".fmt(this.get("name")));
     },
-    incorrectlyMocked: function() {
+    _logIncorrectRequests: function(incorrect) {
         var self = this;
-        this.get("incorrectMocks").forEach(function(request){
+        incorrect.forEach(function(request){
             self._incorrectMessage(request);
         });
         ok(false, "Incorrectly mocked requests for %@.".fmt(this.get("name")));
@@ -90,14 +106,14 @@ var TestDone = Ember.Object.extend({
         return output;
     },
     _logObjectData: function(name, data) {
+        console.log(name + " data:");
         for(var key in data) {
             if(data.hasOwnProperty(key)) {
-                console.log(name + " data:");
                 console.log(key + ": " + data[key]);
             }
         }
     },
-    createMessage: function(request, verb) {
+    _createMessage: function(request, verb) {
         var message = [
             "Request:",
             request.type,
--- tests/unit/fauxjax-testdone-test.js
+++ tests/unit/fauxjax-testdone-test.js
@@ -1,7 +1,7 @@
 /*global ok:true */
 import TestDone from "ember-cli-test-helpers/tests/helpers/test-done";
 
-var originalOk, originalWarn, done;
+var originalOk, originalWarn, originalLog, done;
 
 module("fauxjaxTestdone", {
     setup: function() {
@@ -12,21 +12,24 @@ module("fauxjaxTestdone", {
         originalOk = ok;
         ok = function() {};
         originalWarn = console.warn;
+        originalLog = console.log;
         console.warn = function() {};
+        console.log = function() {};
         $.fauxjax.unfired = function() { return []; };
         $.fauxjax.unhandled = function() { return []; };
     },
     teardown: function() {
         ok = originalOk;
         console.warn = originalWarn;
+        console.log = originalLog;
     }
 });
 
-test("no unfired requests and no unmocked requests returns 0", function() {
-    equal(done.testDoneCallback(), undefined);
+test("no unfired requests and no unmocked requests returns undefined", function(assert) {
+    assert.equal(done.testDoneCallback(), undefined);
 });
 
-test("overMocked called when unfired requests", function() {
+test("'unfired' returned when there are unfired requests", function(assert) {
     var unfired = [
         {
             url: "/foo",
@@ -34,10 +37,10 @@ test("overMocked called when unfired requests", function() {
         }
     ];
     $.fauxjax.unfired = function() { return unfired; };
-    equal(done.testDoneCallback(), "unfired");
+    assert.equal(done.testDoneCallback(), "unfired");
 });
 
-test("underMocked called when unhandled requests", function() {
+test("'unhandled' when there are unhandled requests", function(assert) {
     var unhandled = [
         {
             url: "/foo",
@@ -45,10 +48,10 @@ test("underMocked called when unhandled requests", function() {
         }
     ];
     $.fauxjax.unhandled = function() { return unhandled; };
-    equal(done.testDoneCallback(), "unhandled");
+    assert.equal(done.testDoneCallback(), "unhandled");
 });
 
-test("overMocked and underMocked called when both unhandled and unfired requests", function() {
+test("'unfired and unhandled' returned there are when both unhandled and unfired requests", function(assert) {
     var unfired = [
         {
             url: "/bar",
@@ -63,10 +66,10 @@ test("overMocked and underMocked called when both unhandled and unfired requests
     ];
     $.fauxjax.unfired = function() { return unfired; };
     $.fauxjax.unhandled = function() { return unhandled; };
-    equal(done.testDoneCallback(), "unfired and unhandled");
+    assert.equal(done.testDoneCallback(), "unfired and unhandled");
 });
 
-test("incorrect mock/request when same url and type on request of unfired and unhandled", function() {
+test("incorrect mock/request when same url and type on request of unfired and unhandled", function(assert) {
     var unfired = [
         {
             url: "/foo",
@@ -83,10 +86,10 @@ test("incorrect mock/request when same url and type on request of unfired and un
     ];
     $.fauxjax.unfired = function() { return unfired; };
     $.fauxjax.unhandled = function() { return unhandled; };
-    equal(done.testDoneCallback(), "incorrect");
+    assert.equal(done.testDoneCallback(), "incorrect");
 });
 
-test("incorrect, unfired, and unhandled requests properly handled", function() {
+test("incorrect, unfired, and unhandled requests properly handled", function(assert) {
     var unfired = [
         {
             url: "/foo",
@@ -111,10 +114,80 @@ test("incorrect, unfired, and unhandled requests properly handled", function() {
     ];
     $.fauxjax.unfired = function() { return unfired; };
     $.fauxjax.unhandled = function() { return unhandled; };
-    equal(done.testDoneCallback(), "incorrect, unfired, and unhandled");
+    assert.equal(done.testDoneCallback(), "incorrect, unfired, and unhandled");
 });
 
-test("incorrectMessage returns 'incorrect data' when stubbed data does not match actual data", function() {
+test("_configureLists will properly sort out unfired, unhandled, and incorrect", function(assert) {
+    var unfired = [
+        {
+            url: "/foo",
+            type: "POST",
+            data: {foo: "bar"}
+        },
+        {
+            url: "/bar",
+            type: "GET"
+        }
+    ];
+    var unhandled = [
+        {
+            url: "/foo",
+            type: "POST",
+            data: {foo: "baz"}
+        },
+        {
+            url: "/wat",
+            type: "GET"
+        },
+        {
+            url: "/baz",
+            type: "GET"
+        }
+    ];
+    $.fauxjax.unfired = function() { return unfired; };
+    $.fauxjax.unhandled = function() { return unhandled; };
+    var lists = done._configureLists();
+    assert.equal(lists.incorrect.length, 1);
+    assert.equal(lists.unfired.length, 1);
+    assert.equal(lists.unhandled.length, 2);
+});
+
+test("_createMessage will be called only for the lists created from _configureLists", function(assert) {
+    var unfired = [
+        {
+            url: "/foo",
+            type: "POST",
+            data: {foo: "bar"}
+        },
+        {
+            url: "/bar",
+            type: "GET"
+        }
+    ];
+    var unhandled = [
+        {
+            url: "/foo",
+            type: "POST",
+            data: {foo: "baz"}
+        },
+        {
+            url: "/wat",
+            type: "GET"
+        },
+        {
+            url: "/baz",
+            type: "GET"
+        }
+    ];
+    $.fauxjax.unfired = function() { return unfired; };
+    $.fauxjax.unhandled = function() { return unhandled; };
+    var counter = 0;
+    done._createMessage = function() { counter += 1; };
+    done.testDoneCallback();
+    assert.equal(counter, 3);
+});
+
+test("incorrectMessage returns 'incorrect data' when stubbed data does not match actual data", function(assert) {
     var request = {
         unfired: {
             url: "/foo",
@@ -128,6 +201,6 @@ test("incorrectMessage returns 'incorrect data' when stubbed data does not match
         }
     };
     var output = done._incorrectMessage(request);
-    equal(output.length, 1);
-    equal(output[0].error, "data");
+    assert.equal(output.length, 1);
+    assert.equal(output[0].error, "data");
 });
