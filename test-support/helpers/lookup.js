f018deacc2cbe8e1e1a1a65cd575c15c2d6d691c
Brandon Williams <williamsbdev@gmail.com>
2015-04-18 21:01:27 -0500

    [FEATURE] make the initializer to expose container part of the test-helpers addon

--- test-support/helpers/lookup.js
+++ test-support/helpers/lookup.js
@@ -1,3 +1,12 @@
+import Application from '../../app';
+
+Application.initializer({
+    name: 'ember-cli-test-helpers-extract-container',
+    initialize: function(c) {
+        window.__container__ = c;
+    }
+});
+
 var lookup = function(name) {
     var container = window.__container__;
     return container.lookup(name);
