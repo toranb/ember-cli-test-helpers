881b3e0a8f76cf40d374638b927e84541e6b2a58
Brandon Williams <williamsbdev@gmail.com>
2015-04-30 22:06:54 -0500

    [FEATURE] support 1.X version of fauxjax API

--- test-support/helpers/stub.js
+++ test-support/helpers/stub.js
@@ -2,14 +2,16 @@ import Ember from "ember";
 
 var stubEndpointForHttpRequest = function(url, json, verb, status, post_data) {
     return Ember.$.fauxjax.new({
-        type: verb || "GET",
-        url: url,
-        status: status || 200,
-        dataType: "json",
-        contentType: "application/json",
-        responseText: json,
-        data: post_data,
-        cache: false
+        request: {
+            url: url,
+            method: verb || "GET",
+            data: post_data,
+            cache: false
+        },
+        response: {
+            status: status || 200,
+            content: json
+        }
     });
 };
 
