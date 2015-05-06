ffa55e440d37ac025c10c9852f3ea88d1dd72216
Brandon Williams <williamsbdev@gmail.com>
2015-05-06 07:36:23 -0500

    [FEATURE] supporting fauxjax 1.X series API for QUnit.testDone callback

--- test-support/helpers/test-done.js
+++ test-support/helpers/test-done.js
@@ -12,15 +12,14 @@ var TestDone = Ember.Object.extend({
         var self = this;
         fauxjax.unfired().forEach(function(unfired){
             var found = fauxjax.unhandled().filter(function(unhandled){
-                return unfired.url === unhandled.url &&
-                    unfired.type === unhandled.type;
+                return self._unfiredAndUnhandledMatch(unfired, unhandled);
             });
             if(found.length === 0) {
-                self._logUnfiredRequests(unfired);
+                self._logUnfiredRequests(unfired.request);
             } else {
                 self._logIncorrectRequests({
-                    unfired: unfired,
-                    unhandled: found[0]
+                    unfired: unfired.request,
+                    unhandled: found[0].request
                 });
             }
         });
@@ -29,35 +28,34 @@ var TestDone = Ember.Object.extend({
         var self = this;
         fauxjax.unhandled().forEach(function(unhandled){
             var found = fauxjax.unfired().filter(function(unfired){
-                return unfired.url === unhandled.url &&
-                    unfired.type === unhandled.type;
+                return self._unfiredAndUnhandledMatch(unfired, unhandled);
             });
             if(found.length === 0) {
-                self._logUnhandledRequests(unhandled);
+                self._logUnhandledRequests(unhandled.request);
             }
         });
     },
+    _unfiredAndUnhandledMatch: function(unfired, unhandled) {
+        return unfired.request.url === unhandled.request.url &&
+            unfired.request.type === unhandled.request.type;
+    },
     _logUnfiredRequests: function(request) {
         this._createMessage(request, "FIRED");
-        QUnit.assert.ok(false, "Overmocked requests for %@.".fmt(this.get("name")));
+        QUnit.assert.ok(false, "Overmocked requests for " + this.get("name"));
     },
     _logUnhandledRequests: function(request) {
         this._createMessage(request, "MOCKED");
-        QUnit.assert.ok(false, "Unmocked requests for %@.".fmt(this.get("name")));
+        QUnit.assert.ok(false, "Unmocked requests for %@." + this.get("name"));
     },
     _logIncorrectRequests: function(request) {
         this._incorrectMessage(request.unfired, request.unhandled);
-        QUnit.assert.ok(false, "Incorrectly mocked requests for %@.".fmt(this.get("name")));
+        QUnit.assert.ok(false, "Incorrectly mocked requests for %@." + this.get("name"));
     },
     _incorrectMessage: function(unfired, unhandled) {
         this._createMessage(unfired, "CORRECT");
         if(unfired.data && !unhandled.data || _.some(_.compact([unfired.data, unhandled.data])) && !_.isEqual(unhandled.data, unfired.data)) {
             this._logObjectData("Mocked data:", unfired.data);
             this._logObjectData("Real Request data:", unhandled.data);
-        }
-        if((unfired.contentType || unhandled.contentType) && !_.isEqual(unfired.contentType, unhandled.contentType)) {
-            console.warn("Mocked Content Type: " + unfired.contentType);
-            console.warn("Real Request Content Type: " + unhandled.contentType);
         }
         if(!_.isEqual(unfired.headers, unhandled.headers)) {
             this._logObjectData("Mocked headers:", unfired.headers);
@@ -87,10 +85,11 @@ var TestDone = Ember.Object.extend({
         console.warn(testNameMessage.join(" "));
         var message = [
             "Request:",
-            request.type,
+            request.type || request.method,
             "to",
             request.url,
-            "not %@".fmt(verb)
+            "not",
+            verb
         ];
         console.warn(message.join(" "));
     }
