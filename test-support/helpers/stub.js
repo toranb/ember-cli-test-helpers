6683596caced155c4956f2e3488d7aaa8a783968
Brandon Williams <williamsbdev@gmail.com>
2015-07-18 20:49:04 -0500

    [ENHANCEMENT] updating API to be more extensible and easier to remember

--- test-support/helpers/stub.js
+++ test-support/helpers/stub.js
@@ -1,5 +1,20 @@
 import Ember from "ember";
 
+var stubRequest = function(request, response) {
+    return Ember.$.fauxjax.new({
+        request: {
+            url: request.url,
+            data: request.data,
+            method: request.method || "GET",
+            cache: false
+        },
+        response: {
+            status: response.status || 200,
+            content: response.json
+        }
+    });
+};
+
 var stubEndpointForHttpRequest = function(url, json, verb, status, post_data) {
     return Ember.$.fauxjax.new({
         request: {
@@ -15,6 +30,14 @@ var stubEndpointForHttpRequest = function(url, json, verb, status, post_data) {
     });
 };
 
+var clearRequests = function(id) {
+    if (id) {
+        Ember.$.fauxjax.remove(id);
+    } else {
+        Ember.$.fauxjax.clear();
+    }
+};
+
 var clearStubHttpRequests = function(id) {
     if (id) {
         Ember.$.fauxjax.remove(id);
@@ -23,4 +46,4 @@ var clearStubHttpRequests = function(id) {
     }
 };
 
-export { stubEndpointForHttpRequest, clearStubHttpRequests };
+export { stubEndpointForHttpRequest, clearStubHttpRequests, stubRequest, clearRequests };
