7684284c51b7f367fa07c9a50e74fcdab61b9afd
Brandon Williams <williamsbdev@gmail.com>
2015-07-22 21:13:59 -0500

    [TEST] adding tests around the stub helper

--- tests/unit/stub-test.js
+++ tests/unit/stub-test.js
@@ -1,32 +1,54 @@
-import { test } from "qunit";
-import QUnit from "qunit";
-import module from "../helpers/module";
+import Ember from "ember";
+import { test, module } from "qunit";
 import {stubRequest} from "../helpers/stub";
 
-var originalOk, okCounter;
 
 module("Unit: Stub Tests", {
-    beforeEach: function() {
-        okCounter = 0;
-        originalOk = QUnit.assert.ok;
-        QUnit.assert.ok = function() { okCounter += 1; };
-    },
-    afterEach: function(assert) {
-        QUnit.assert.ok = originalOk;
-        // This is asserting all stubs for requests are properly handled
-        assert.equal(okCounter, 0);
+    afterEach: function() {
+        Ember.$.fauxjax.clear();
     }
 });
 
-test('module will not fail if stub does not specify response.content', function(assert) {
+test("stubRequest requires a request.url", function(assert) {
+    try {
+        stubRequest({});
+    } catch(e) {
+        assert.equal(e.message, "Please provide a url for the request");
+    }
+});
+
+test("stubRequest will set request.cache to false by default", function(assert) {
+    stubRequest({url: "/wat", method: "GET"});
+    var stub = Ember.$.fauxjax.unfired()[0];
+    assert.equal(false, stub.request.cache);
+});
+
+test("stubRequest will respect request.cache if set", function(assert) {
+    stubRequest({url: "/wat", method: "GET", cache: true});
+    var stub = Ember.$.fauxjax.unfired()[0];
+    assert.equal(true, stub.request.cache);
+});
+
+test("stubRequest will set request.method to 'GET' by default", function(assert) {
+    stubRequest({url: "/wat"});
+    var stub = Ember.$.fauxjax.unfired()[0];
+    assert.equal("GET", stub.request.method);
+});
+
+test("stubRequest will respect request.method if set", function(assert) {
     stubRequest({url: "/wat", method: "POST"});
-    $.ajax({
-        method: "POST",
-        url: "/wat"
-    });
-    stubRequest({url: "/foo"});
-    $.ajax({
-        method: "GET",
-        url: "/foo"
-    });
+    var stub = Ember.$.fauxjax.unfired()[0];
+    assert.equal("POST", stub.request.method);
+});
+
+test("stubRequest will set response.status to 200 by default", function(assert) {
+    stubRequest({url: "/wat"});
+    var stub = Ember.$.fauxjax.unfired()[0];
+    assert.equal(200, stub.response.status);
+});
+
+test("stubRequest will respect response.status if set", function(assert) {
+    stubRequest({url: "/wat", method: "POST"}, {status: 201});
+    var stub = Ember.$.fauxjax.unfired()[0];
+    assert.equal(201, stub.response.status);
 });
